d10fb063292d3ddb5d3a8bce827c591f8dacf720
NewYoung <657851574@qq.com>
2020-07-18 22:08:43 +0800

    小修改

--- index.js
+++ index.js
@@ -40,21 +40,57 @@
   }
 
   SimplePromise.prototype.then = function (onFulfilled, onRejected) {
-    switch (this.status) {
-      case PENDING:
-        this.fulfilledList.push(onFulfilled);
-        this.rejectedList.push(onRejected);
-        break;
-      case FULFILLED:
-        onFulfilled(this.value);
-        break;
-      case REJECTED:
-        onRejected(this.value);
-        break;
-      default:
-        break;
-    }
-    return this;
+    let self = this;
+    return new SimplePromise(function (resolve, reject) {
+      let fulfilled = function (val) {
+        try {
+          if (!isFunction(onFulfilled)) { //非函数
+            resolve(val) //立即执行
+          } else {
+            let res = onFulfilled(val);
+            if (res instanceof SimplePromise) { //上一个回调返回 promise，则下一个回调必须等待其状态发生变换后才能执行
+              res.then(resolve, reject);
+            } else { //将上一个回调函数的返回值作为下一个回调的参数且立即执行
+              resolve(res)
+            }
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }
+
+      let rejected = function (err) {
+        try {
+          if (!isFunction(onRejected)) {
+            reject(err)
+          } else {
+            let res = onRejected(err);
+            if (res instanceof SimplePromise) {
+              res.then(resolve, reject)
+            } else {
+              reject(res)
+            }
+          }
+        } catch (error) {
+          reject(error);
+        }
+      }
+
+      switch (self.status) {
+        case PENDING:
+          self.fulfilledList.push(fulfilled); //新的 promise 和上一个 promise 相关联
+          self.rejectedList.push(rejected);
+          break;
+        case FULFILLED:
+          fulfilled(self.value);
+          break;
+        case REJECTED:
+          rejected(self.value);
+          break;
+        default:
+          break;
+      }
+    })
   }
 
   SimplePromise.prototype.resolve = function (data) {
